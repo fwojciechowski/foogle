c4df6d79635b80285578f671150aa9407db4ebfe
Filip Wojciechowski <filip-wojciechowski@o2.pl>
2019-01-10 21:50:58 +0100

    Comments. Moved looking for phrase to the outside function.

--- main.c
+++ main.c
@@ -12,16 +12,20 @@
 char bibliotekaPath[200];
 const int gRecordsSize = 3000; // maksymalna liczba rekordow do wykorzystania
 
+// Struktura do trzymania nazw plikow z biblioteki oraz sciezki do nich.
 struct Record {
     char name[100];
     char fullPath[200];
 };
 
+// Struktura do trzymania wynikow - nazwa pliku oraz ilosc wystapien frazy w danym pliku.
 struct Result {
     char name[100];
     int hits;
 };
 
+// Inicjalizacja biblioteki
+// Funkcja zczytuje z podanej lokalizacji liste plikow, w ktorym odbeda sie poszukiwania zadanej frazy
 int initializeLibrary(struct Record library[], int* size) {
     int status = 0;
     DIR *biblioteka;
@@ -31,7 +35,7 @@ int initializeLibrary(struct Record library[], int* size) {
 
     if (biblioteka) {
         while ((dir = readdir(biblioteka)) != NULL && (*size) < gRecordsSize) {
-            if (dir->d_type != DT_REG) continue; // katalogi nas nie interesuja
+            if (dir->d_type != DT_REG) continue; // interesujace sa tylko zwykle pliki
 
             strcpy(library->name, dir->d_name);
             strcpy(library->fullPath, bibliotekaPath);
@@ -48,50 +52,15 @@ int initializeLibrary(struct Record library[], int* size) {
     return status;
 }
 
-int findPhrase() {
-
-}
-
-int main(int argc, char *argv[]) {
-    struct Record records[gRecordsSize];
-    int recordsSize = 0;
-    struct Result results[gRecordsSize];
-    struct Result *r = results;
-    int resultsSize = 0;
-    char phrase[256];
-
-    if ( argc != 3 ) {
-        printf("Program potrzebuje lokalizacji biblioteki oraz frazy do wyszukiwania w bibliotece.\n\n");
-        printf("Przykład:\n./foogle ./biblioteka pomidor\n");
-        return EXIT_FAILURE;
-    }
-
-    strcpy(bibliotekaPath, argv[1]);
-    if (bibliotekaPath[ strlen(bibliotekaPath) - 1 ] != '/') {
-        strcat(bibliotekaPath, "/");
-    }
-
-    strcpy(phrase, argv[2]);
-
-    if (initializeLibrary(records, &recordsSize) == 1) {
-        printf("Błąd przy inicjalizacji biblioteki. Prawdopodobnie program nie odnalazł podanej ścieżki.\n\n");
-        return EXIT_FAILURE;
-    }
-
-    double beginTime = omp_get_wtime();
-
-    printf("foogle - wyszukiwarka fraz w bibliotece\n\n");
-
-    printf("Szukanie frazy: " KYEL "%s" KNRM "\n\n", argv[2]);
-
-    printf("Przeszukiwanie %d rekordów...\n\n", recordsSize);
-
-    // Glowna petla for do przyszlego zrownoleglenia.
+// Znajdywanie frazy
+// Glowna petla programu. Na podstawie bazy plikow i frazy funkcja przeszukuje biblioteke i zwraca wynik
+// do tablicy struktur Result (poprzez wskaznik r).
+void findPhrase(const struct Record records[], const int* recordsSize, struct Result* r, int* resultsSize, const char phrase[]) {
     char line[256];
     int count;
     FILE* file;
-    #pragma omp parallel for shared(r, resultsSize, records, phrase) private(file, line, count) num_threads(4)
-    for (int i = 0; i < recordsSize; i++) {
+#pragma omp parallel for shared(r, resultsSize, records, phrase) private(file, line, count) num_threads(4)
+    for (int i = 0; i < *recordsSize; i++) {
 
         count = 0;
 
@@ -105,11 +74,11 @@ int main(int argc, char *argv[]) {
             }
 
             if (count) {
-                #pragma omp critical
+#pragma omp critical
                 {
                     r->hits = count;
                     strcpy(r->name, records[i].name);
-                    resultsSize++;
+                    (*resultsSize)++;
                     r++;
                 };
             }
@@ -117,6 +86,44 @@ int main(int argc, char *argv[]) {
             fclose(file);
         }
     }
+}
+
+int main(int argc, char *argv[]) {
+    struct Record records[gRecordsSize];
+    int recordsSize = 0;
+    struct Result results[gRecordsSize];
+    struct Result *r = results;
+    int resultsSize = 0;
+    char phrase[256];
+
+    // Sprawdzamy, czy lokalizacja biblioteki oraz szukana fraza zostaly podane.
+    if ( argc != 3 ) {
+        printf("Program potrzebuje lokalizacji biblioteki oraz frazy do wyszukiwania w bibliotece.\n\n");
+        printf("Przykład:\n./foogle ./biblioteka pomidor\n");
+        return EXIT_FAILURE;
+    }
+
+    strcpy(bibliotekaPath, argv[1]);
+    if (bibliotekaPath[ strlen(bibliotekaPath) - 1 ] != '/') {
+        strcat(bibliotekaPath, "/");
+    }
+
+    strcpy(phrase, argv[2]);
+
+    // Biblioteka wymaga inicjalizacji tj. utworzenia listy znajdujacych sie w niej plikow.
+    if (initializeLibrary(records, &recordsSize) == 1) {
+        printf("Błąd przy inicjalizacji biblioteki. Prawdopodobnie program nie odnalazł podanej ścieżki.\n\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("foogle - wyszukiwarka fraz w bibliotece\n\n");
+    printf("Szukanie frazy: " KYEL "%s" KNRM "\n\n", phrase);
+    printf("Przeszukiwanie %d rekordów...\n\n", recordsSize);
+
+    double beginTime = omp_get_wtime();
+
+    // Serce programu - wyszukiwanie frazy w bibliotece.
+    findPhrase(records, &recordsSize, r, &resultsSize, phrase);
 
     double endTime = omp_get_wtime();
 
